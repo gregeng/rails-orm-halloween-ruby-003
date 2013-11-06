f66b3d0bee8b7470aa3f9aff1ff0188674a1e90d
Greg Eng <greg.eng@gmail.com>
2013-11-06 15:34:32 -0500

    initial commit, set up all project folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsOrmHalloweenRuby003::Application.config.secret_key_base = '49d5aede9b96b85f9265e03911b7067087ab9245c9f46b42c9afc1f9b5a75d5d01a6d92ad610314d57a3aec8d66dd0addc378097b705acbef2018042b57cd986'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsOrmHalloweenRuby003::Application.config.session_store :cookie_store, key: '_rails-orm-halloween-ruby-003_session'
